b1a59ad9e5852d3b9d88b24d5b1fa303f7ce74dd
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2026-04-07 18:08:00 +0100

    design e layout da area do paciente finalizado

--- frontend/src/components/areadopaciente.tsx
+++ frontend/src/components/areadopaciente.tsx
@@ -11,7 +11,7 @@ export default function AreaDoPaciente() {
         </span>
       </nav>
 
-      <nav className="flex flex-col md:flex-row w-full md:w-[70%] sm:w-[80%] p-8 bg-white rounded-md shadow-md mx-auto  sm:items-center sm:justify-center ">
+      <nav className="flex flex-col md:flex-row w-full md:w-[70%] sm:w-[80%] p-8 bg-white rounded-md shadow-md mx-auto md:items-start sm:items-center sm:justify-center  ">
         
         {/* Esquerda */}
         <section className="flex flex-col w-1/2 md:items-start ">
@@ -20,7 +20,7 @@ export default function AreaDoPaciente() {
           <label htmlFor="email" className="font-medium">E-mail</label>
           <input
             id="email"
-            className="px-3 w-full max-w-sm py-2 mt-2 font-semibold text-[14px] text-[#6B7280] 
+            className="px-3 w-full py-2 mt-2 font-semibold text-[14px] text-[#6B7280] 
                        border border-gray-300 rounded-md 
                        focus:outline-none focus:ring-2 focus:ring-[#2BAE66] focus:border-[#2BAE66]"
             placeholder="seu@email.com"
@@ -30,13 +30,40 @@ export default function AreaDoPaciente() {
           <label htmlFor="senha" className="font-medium mt-5">Senha</label>
           <input
             id="senha"
-            className="px-3 w-full max-w-sm py-2 mt-2 font-semibold text-[14px] text-[#6B7280] 
+            className="px-3 w-full py-2 mt-2 font-semibold text-[14px] text-[#6B7280] 
                        border border-gray-300 rounded-md 
                        focus:outline-none focus:ring-2 focus:ring-[#2BAE66] focus:border-[#2BAE66]"
             placeholder="*******"
             type="password"
           />
+
+                { /* Lembrar-me e Esqueci minha senha lado a lado */}
+
+        <section className="flex flex-row w-full justify-between items-center  mt-3">
+
+         <div className="flex flex-row w-full justify-between items-center">
+
+         <nav >
+             <input type="checkbox" id="lembrar" className="mr-2"/>
+             <label htmlFor="lembrar" className="text-sm text-[#6B8280]">Lembrar-me</label>
+         </nav>
+             <nav>
+              <a href="#" className="text-sm text-[#2BAE66] ml-auto">Esqueci minha senha</a>
+            </nav>
+        
+         </div>
         </section>
+
+      <div className="flex text-center items-center justify-center w-full">
+          <button className="w-full md:py-2 sm:py-1 mt-3 rounded-sm bg-[#2BAE66] text-white">Entrar na conta  </button>
+      </div>
+
+        </section>
+
+                {/* Linha de separação vertical */}
+        
+        <div className="hidden md:block w-px bg-gray-300 mx-8 self-stretch"></div>
+
 
         {/* Direita */}
         <section className="flex flex-col w-1/2 justify-center md:items-start ">
@@ -45,7 +72,7 @@ export default function AreaDoPaciente() {
            <label htmlFor="nome" className="font-medium">Nome Completo</label>
           <input
             id="nome"
-            className="px-3 w-full py-2 mt-2 font-semibold text-[14px] text-[#6B7280] 
+            className="px-3 w-full py-2 mt-2 mb-2 font-semibold text-[14px] text-[#6B7280] 
                        border border-gray-300 rounded-md 
                        focus:outline-none focus:ring-2 focus:ring-[#2BAE66] focus:border-[#2BAE66]"
             placeholder="Ex: Maria Joaquina Silva"
@@ -90,7 +117,12 @@ export default function AreaDoPaciente() {
     />
   </div>
 
+
 </section>
+
+  <div className="flex text-center items-center justify-center w-full">
+     <button className="w-full md:py-2 sm:py-1 mt-3 rounded-sm bg-[#b6b8ba] text-[#0F1720] cursor-pointer">Criar minha conta  </button>
+  </div>
 
         </section>
 
